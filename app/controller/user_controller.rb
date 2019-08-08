d4893d4c8b0742d8c265a912e9a4f2fb88c6ea97
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-08 18:10:14 +0000

    login

--- app/controller/user_controller.rb
+++ app/controller/user_controller.rb
@@ -5,9 +5,9 @@ class UserController < ApplicationController
   
   post '/login' do 
     user = User.find_by(username: params[:username])
-    if !!user
+    if !!user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect :'posts/index'
+      erb :index
     else 
       erb :'sessions/login'
     end 
