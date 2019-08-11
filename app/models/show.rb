d7644ab054ff68be963d43b56954b1b061792bca
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-11 18:45:02 +0000

    testing shows on user

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,3 @@
-class Shows < ActiveRecord::Base 
+class Show < ActiveRecord::Base 
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_164157) do
+ActiveRecord::Schema.define(version: 2019_08_11_183859) do
+
+  create_table "shows", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
