78ad143e0ad3853e2c920f1f353ce565df3a8526
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-11 20:42:08 +0000

    delete and show

--- app/controller/shows_controller.rb
+++ app/controller/shows_controller.rb
@@ -8,4 +8,14 @@ class ShowsController < ApplicationController
   get '/shows/new' do 
     erb :'shows/new' 
   end
+  
+  post '/shows' do
+    @show = Show.create(params)
+    redirect to "/shows/#{@show.id}"
+  end
+  
+  get '/shows/:id' do
+    @show = Show.find(params[:id])
+    erb :'shows/show'
+  end
 end
